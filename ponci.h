815542b3fac0cbbbc16f2f6daf678fc7e5725e4a
Jens Breitbart <jbreitbart@gmail.com>
2016-03-08 16:22:14 +0100

    Updated license text in the files.

--- ponci.h
+++ ponci.h
@@ -5,7 +5,7 @@
  * Copyright 2016 by LRR-TUM
  * Jens Breitbart     <j.breitbart@tum.de>
  *
- * Licensed under GNU General Public License 2.0 or later.
+ * Licensed under GNU Lesser General Public License 2.1 or later.
  * Some rights reserved. See LICENSE
  */
 
